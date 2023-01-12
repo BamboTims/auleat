7413be4b7c262e5cd958e53b2bebe0e3b503000b
Bambotims <timilehinadebambo@gmail.com>
2023-01-12 22:57:49 +0100

    initial commit

---
+++ assets/js/cart.js
@@ -0,0 +1,41 @@
+const entries = document?.querySelector(".entries");
+const orders = document.querySelector(".title-checkout").querySelector("h6");
+const prices = document.querySelectorAll(".prio");
+const carts = JSON.parse(sessionStorage.getItem("carts"));
+
+orders.innerText = carts.length;
+const total = carts.reduce((acc, el) => el.quantity * el.price + acc, 0);
+console.log(total);
+
+prices.forEach((el) => {
+  el.innerText = `N${total}`;
+});
+
+carts.forEach((el) => {
+  const html = `<li class="price-list">
+    <i class="closeButton fa-solid fa-xmark"></i>
+    <div class="counter-container">
+      <div class="counter-food">
+        <h4>${el.foodname}</h4>
+      </div>
+    </div>
+    <div class="price">
+        <div>
+                  <h2>N${el.price * el.quantity}</h2>
+                  <span>Sum</span>
+                </div>
+                  <div>
+                    <div class="qty-input">
+              <button class="qty-count qty-count--minus" data-action="minus" type="button">-</button>
+              <input class="product-qty" type="number" name="product-qty" min="0" value=${
+                el.quantity
+              }>
+              <button class="qty-count qty-count--add" data-action="add" type="button">+</button>
+            </div>
+            <span>Quantity</span>
+          </div>
+          </div>
+    </li>`;
+
+  entries.insertAdjacentHTML("beforeend", html);
+});
---
+++ assets/js/meal.js
@@ -0,0 +1,124 @@
+"use strict";
+
+const container = document.querySelector("#container");
+const carts = [];
+
+const meals = [
+  {
+    foodname: "Toast and egg",
+    price: 50,
+    mealtype: "breakfast",
+    image:
+      "https://unsplash.com/photos/FFqNATH27EM/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8NHx8YnJlYWtmYXN0fGVufDB8fHx8MTY3MzA3NDMxMA&force=true&w=640",
+  },
+
+  {
+    foodname: "Oatmeal and banana",
+    price: 60,
+    mealtype: "breakfast",
+    image:
+      "https://unsplash.com/photos/oh-5xHAtJg8/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fEJyZWFrZmFzdHxlbnwwfHx8fDE2NzMwNzQ1NTY&force=true&w=640",
+  },
+  {
+    foodname: "Efo riro and Semovita",
+    price: 70,
+    mealtype: "Any",
+    image:
+      "https://unsplash.com/photos/kn_ANxnwCQ0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8bmlnZXJpYW4lMjBmb29kfGVufDB8fHx8MTY3MzAzNjAzMw&force=true&w=640",
+  },
+  {
+    foodname: "White rice fried egg and beef",
+    price: 10,
+    mealtype: "Lunch",
+    image:
+      "https://unsplash.com/photos/szQFiGZH8wU/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8am9sbG9mJTIwcmljZXxlbnwwfHx8fDE2NzMwMzM4NzA&force=true&w=640",
+  },
+  {
+    foodname: "Indomie and fried egg",
+    price: 45,
+    mealtype: "Lunch",
+    image:
+      "https://unsplash.com/photos/rn7BK2AaaOE/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTR8fGpvbGxvZiUyMHJpY2V8ZW58MHx8fHwxNjczMDMzODcw&force=true&w=640",
+  },
+];
+
+``;
+
+meals.forEach((el) => {
+  const html = `<div class="col-xl-4 col-lg-6" data-aos="flip-up"  data-aos-delay="200" data-aos-duration="300">
+  <div class="dish">
+  <img alt="food-dish" src=${el.image}>
+  <div class="dish-foods">
+    <h3>${el.foodname}</h3>
+<div class="dish-icon">
+ <div class="cafa-button">
+     <a href="#">${el.mealtype}</a>
+     <a href="#">Brunch</a>
+  </div>
+  <div class="dish-icon end">
+
+    <i class="info fa-solid fa-circle-info"></i>
+    <div class="star">
+      <a href="#"><i class="fa-solid fa-heart"></i></a>
+    </div>
+  </div>
+     </div>
+     <div class="price">
+       <h2>N${el.price}</h2>
+       <div class="qty-input">
+   <button class="qty-count qty-count--minus" data-action="minus" type="button">-</button>
+   <input class="product-qty" type="number" name="product-qty" min="0" value="1">
+   <button class="qty-count qty-count--add" data-action="add" type="button">+</button>
+ </div>
+</div>
+<button class="button-price">Add to Basket<i class="fa-solid fa-bag-shopping"></i></button>
+    </div>
+    <div class="dish-info" style="display: none;">
+      <i class="info2 fa-solid fa-xmark"></i>
+      <h5>
+        ${el.foodname}
+      </h5>
+      <div class="cafa-button">
+     <a href="#">${el.mealtype}</a>
+     <a href="#">Brunch</a>
+</div>
+<p>In egestas erat imperdiet sed euismod nisi porta. Ultrices sagittis orci a scelerisque. Diam quam nulla porttitor.</p>
+<ul class="menu-dish">
+  <li>Nulla porttitor massa id;</li>
+  <li>Aliquam vestibulum morbi;</li>
+  <li>Blandit donec adipiscing;</li>
+</ul>
+    </div>
+</div></div></div>`;
+
+  container.insertAdjacentHTML("beforeend", html);
+});
+
+container.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".button-price");
+
+  if (!clicked) return;
+
+  const parent = clicked.parentNode;
+  const foodName = parent.children[0].innerText;
+  const quantity = parent.querySelector(".product-qty").value;
+
+  const foodItem = meals.find((el) => el.foodname === foodName);
+
+  const isInCart = carts.includes(carts.find((el) => el.foodname === foodName));
+
+  if (isInCart) {
+    const foodItemIndex = carts.indexOf(
+      carts.find((el) => el.foodname === foodName)
+    );
+    carts[foodItemIndex].quantity += +quantity;
+  } else {
+    carts.push({
+      quantity: 1,
+      ...foodItem,
+    });
+  }
+
+  sessionStorage.clear();
+  sessionStorage.setItem("carts", JSON.stringify(carts));
+});
