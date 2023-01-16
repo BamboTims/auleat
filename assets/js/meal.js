26e66c50c7749f74146256c7a2b5718291f8ba01
Timibambo <timilehinadebambo@gmail.com>
2023-01-16 09:37:13 +0100

    Update meal.js

--- assets/js/meal.js
+++ assets/js/meal.js
@@ -38,7 +38,7 @@ const breakFast = [
   },
   {
     foodname: "Indomie and fried egg",
-    price: 45,
+    price: 150,
     mealtype: "Lunch",
     image:
       "https://unsplash.com/photos/rn7BK2AaaOE/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTR8fGpvbGxvZiUyMHJpY2V8ZW58MHx8fHwxNjczMDMzODcw&force=true&w=640",
@@ -54,58 +54,78 @@ const breakFast = [
 
 const Lunch = [
   {
-    foodname: "Toast and egg",
-    price: 50,
+    foodname: "Spaghetti",
+    price: 100,
     mealtype: "breakfast",
     image:
-      "https://unsplash.com/photos/FFqNATH27EM/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8NHx8YnJlYWtmYXN0fGVufDB8fHx8MTY3MzA3NDMxMA&force=true&w=640",
-  },
-
-  {
-    foodname: "Oatmeal and banana",
-    price: 60,
-    mealtype: "breakfast",
-    image:
-      "https://unsplash.com/photos/oh-5xHAtJg8/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fEJyZWFrZmFzdHxlbnwwfHx8fDE2NzMwNzQ1NTY&force=true&w=640",
+      "https://images.unsplash.com/photo-1622973536968-3ead9e780960?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   },
   {
     foodname: "Efo riro and Semovita",
-    price: 70,
+    price: 150,
     mealtype: "Any",
     image:
       "https://unsplash.com/photos/kn_ANxnwCQ0/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8bmlnZXJpYW4lMjBmb29kfGVufDB8fHx8MTY3MzAzNjAzMw&force=true&w=640",
   },
   {
     foodname: "White rice fried egg and beef",
-    price: 10,
+    price: 300,
     mealtype: "Lunch",
     image:
       "https://unsplash.com/photos/szQFiGZH8wU/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8am9sbG9mJTIwcmljZXxlbnwwfHx8fDE2NzMwMzM4NzA&force=true&w=640",
   },
   {
     foodname: "Indomie and fried egg",
-    price: 45,
+    price: 150,
     mealtype: "Lunch",
     image:
       "https://unsplash.com/photos/rn7BK2AaaOE/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTR8fGpvbGxvZiUyMHJpY2V8ZW58MHx8fHwxNjczMDMzODcw&force=true&w=640",
+  },
+  {
+    foodname: "Jollof rice",
+    price: 100,
+    mealtype: "Lunch",
+    image:
+      "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2022/08/01/jollof-rice-on-blue-and-white-plate.jpg.rend.hgtvcom.476.357.suffix/1659375750019.jpeg",
+  },
+  {
+    foodname: "Beans and Plantain",
+    price: 150,
+    mealtype: "breakfast",
+    image:
+      "https://9jacuisine.co.uk/wp-content/uploads/2021/10/Ewa-agoyin.jpg",
+  },
+   {
+    foodname: "Apple",
+    price: 150,
+    mealtype: "fruits",
+    image:
+      "https://images.unsplash.com/photo-1619546813926-a78fa6372cd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+  },
+   {
+    foodname: "Banana",
+    price: 150,
+    mealtype: "fruits",
+    image:
+      "https://images.unsplash.com/photo-1603052875302-d376b7c0638a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
   },
 ];
 
 const Dinner = [
   {
-    foodname: "Toast and egg",
-    price: 50,
-    mealtype: "breakfast",
+    foodname: "Moi moi",
+    price: 100,
+    mealtype: "dinner",
     image:
-      "https://unsplash.com/photos/FFqNATH27EM/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8NHx8YnJlYWtmYXN0fGVufDB8fHx8MTY3MzA3NDMxMA&force=true&w=640",
+      "https://i0.wp.com/www.cookingwithseki.com/wp-content/uploads/2017/08/IMG_5994.jpg?fit=640%2C640&ssl=1",
   },
 
   {
-    foodname: "Oatmeal and banana",
-    price: 60,
-    mealtype: "breakfast",
+    foodname: "Fried rice",
+    price: 100,
+    mealtype: "dinner",
     image:
-      "https://unsplash.com/photos/oh-5xHAtJg8/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MjN8fEJyZWFrZmFzdHxlbnwwfHx8fDE2NzMwNzQ1NTY&force=true&w=640",
+      "https://cheflolaskitchen.com/wp-content/uploads/2015/06/website-edition-500x455.jpg",
   },
   {
     foodname: "Efo riro and Semovita",
@@ -116,17 +136,24 @@ const Dinner = [
   },
   {
     foodname: "White rice fried egg and beef",
-    price: 10,
-    mealtype: "Lunch",
+    price: 300,
+    mealtype: "dinner",
     image:
       "https://unsplash.com/photos/szQFiGZH8wU/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MXx8am9sbG9mJTIwcmljZXxlbnwwfHx8fDE2NzMwMzM4NzA&force=true&w=640",
   },
   {
     foodname: "Indomie and fried egg",
-    price: 45,
-    mealtype: "Lunch",
+    price: 150,
+    mealtype: "dinner",
     image:
       "https://unsplash.com/photos/rn7BK2AaaOE/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8MTR8fGpvbGxvZiUyMHJpY2V8ZW58MHx8fHwxNjczMDMzODcw&force=true&w=640",
+  },
+   {
+    foodname: "Yam porridge",
+    price: 150,
+    mealtype: "dinner",
+    image:
+      "https://i0.wp.com/www.thepretendchef.com/wp-content/uploads/2017/06/Asaro-mashed-yam-porridge-1-5.jpg?fit=640%2C480&ssl=1",
   },
 ];
 
