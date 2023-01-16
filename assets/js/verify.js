4e8fe895a71aa0bb7fcad310d7319689b0a1a07d
Timibambo <timilehinadebambo@gmail.com>
2023-01-16 09:41:43 +0100

    Update verify.js

--- assets/js/verify.js
+++ assets/js/verify.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const button = document.querySelector(".button-price");
+const btn = document.querySelector("btn-2");
 const form = document.querySelector("form");
 const wrapper = document.querySelector(".wrap");
 const orderNumber = document.querySelector("#orderNo");
@@ -11,4 +12,8 @@ button.addEventListener("click", (e) => {
 
   const orderNo = Math.random() * 9999999;
   orderNumber.innerText = `ON${orderNo} `;
+});
+
+btn.addEventListener("click", (e)=> {
+  wrapper.classList.add("hidden");
 });
